58916dcf6854a07c943d367b60181d04d51dbe28
seven <qiyutao12@163.com>
2016-07-23 11:35:23 +0800

    V1.1

---
+++ app/src/main/java/com/seven/mobilesafe/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.seven.mobilesafe.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.seven.mobilesafe.R;
+
+/**
+ * Created by seven on 7/23/16.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/seven/mobilesafe/ui/SplashActivity.java
+++ app/src/main/java/com/seven/mobilesafe/ui/SplashActivity.java
@@ -1,10 +1,12 @@
 package com.seven.mobilesafe.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
@@ -35,6 +37,8 @@ public class SplashActivity extends Activity {
         AlphaAnimation aa = new AlphaAnimation(0.0f,1.0f);
         aa.setDuration(200);
         main_splash.setAnimation(aa);
+
+        createDlg();
     }
 
     private String getVersion() {
@@ -47,4 +51,28 @@ public class SplashActivity extends Activity {
             return "Unknow Version";
         }
     }
+
+    private void loadMainUI() {
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void createDlg() {
+        AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+
+        dlg.setIcon(R.drawable.icon5);
+        dlg.setTitle("呵呵");
+        dlg.setMessage("what the fuck");
+        dlg.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                loadMainUI();
+            }
+        });
+
+        dlg.create().show();
+    }
+
 }
